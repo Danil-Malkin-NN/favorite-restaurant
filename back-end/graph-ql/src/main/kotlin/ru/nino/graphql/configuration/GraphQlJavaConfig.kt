50833344612a094b1766769a5a4df5b3aaa9b955
Danil-Malkin-NN <danilmalkin2015@gmail.com>
2022-06-15 17:17:08 +0300

    good gradle dependencies https://opensource.expediagroup.com/graphql-kotlin/docs

--- back-end/graph-ql/src/main/kotlin/ru/nino/graphql/configuration/GraphQlJavaConfig.kt
+++ back-end/graph-ql/src/main/kotlin/ru/nino/graphql/configuration/GraphQlJavaConfig.kt
@@ -9,23 +9,23 @@ import ru.nino.db.entity.EatingPoint
 import ru.nino.db.repository.EatingPointRepository
 import java.awt.print.Book
 
-@Configuration
-@ComponentScan("ru.nino.db.repository")
-class GraphQlJavaConfig(
-    @Autowired
-    final val eatingPointRepository: EatingPointRepository
-) {
+//@Configuration
+//@ComponentScan("ru.nino.db.repository")
+//class GraphQlJavaConfig(
 
 
-    val eatingPoints: List<EatingPoint> = eatingPointRepository.findAll()
+@Autowired
+val eatingPointRepository: EatingPointRepository =
 
-    class Query {
-        fun bookById(id: Long): EatingPoint? = eatingPoints.find { it.id == id }
-    }
 
-    val config = SchemaGeneratorConfig(supportedPackages = "com.example")
-    val queries = listOf(TopLevelObject(Query()))
-    val schema: GraphQLSchema = toSchema(config, queries)
+val eatingPoints: List<EatingPoint> = eatingPointRepository.findAll()
 
+class Query {
+    fun bookById(id: Long): EatingPoint = eatingPoints { it.id == id }
+}
+
+val config = SchemaGeneratorConfig(supportedPackages = "ru.nino.db")
+val queries = listOf(TopLevelObject(Query()))
+val schema: GraphQLSchema = toSchema(config, queries)
 }
 
